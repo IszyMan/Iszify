35777b0f97f3a6106e59ecb1019b5c94826b256d
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-04 06:54:07 +0100

    Update profile.py

--- models/profile.py
+++ models/profile.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 class CreateProfile(UserMixin, db.Model):
     __tablename__ = "users_links"
     id = db.Column(db.Integer, primary_key=True)
-    author_name = db.Column(db.Integer, db.ForeignKey("users.name"))
+    author_name = db.Column(db.Integer, db.ForeignKey("users.id"))
     author = relationship("User", back_populates="posts")
     linkname = db.Column(db.String(250), nullable=False)
     yourlink = db.Column(db.String(250), nullable=False)
