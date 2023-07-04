89f0ea0e1d83e0b22dc644e39967a9ec8438cd8b
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-04 06:54:15 +0100

    Update brand.py

--- models/brand.py
+++ models/brand.py
@@ -3,8 +3,9 @@ from extensions import db
 from sqlalchemy.orm import relationship
 
 
-class ChooseBrandName(UserMixin, db.Model):
-    __tablename__ = "brand_name"
-    id = db.Column(db.Integer, primary_key=True)
-    brandname = db.Column(db.String(100), unique=True)
-    brand = relationship("User", back_populates="brand")
+# THIS MODEL NOT IN USE
+# class ChooseBrandName(UserMixin, db.Model):
+#     __tablename__ = "brand_name"
+#     id = db.Column(db.Integer, primary_key=True)
+#     brandname = db.Column(db.String(100), unique=True)
+#     brand = relationship("User", back_populates="brand")
