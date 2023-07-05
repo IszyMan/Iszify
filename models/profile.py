2ac2eb2253d7dd3b7132deb67854efb37fe6028d
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-05 10:21:26 +0100

    product column

--- models/profile.py
+++ models/profile.py
@@ -8,6 +8,7 @@ class CreateProfile(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     author = relationship("User", back_populates="posts")
+    product = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, default="")
     linkname = db.Column(db.String(250), nullable=False)
     yourlink = db.Column(db.String(250), nullable=False)
