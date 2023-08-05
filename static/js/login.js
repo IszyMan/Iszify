f822736b9430ad67d5950ec4f4506073a6ed5aa0
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-08-05 16:02:51 +0100

    update

---
+++ static/js/login.js
@@ -0,0 +1,12 @@
+// document.querySelectorAll('.input-field input').forEach((input) => {
+//     input.addEventListener('focus', function() {
+//       this.parentNode.classList.add('focusInput');
+//     });
+  
+//     input.addEventListener('blur', function() {
+//       if (this.value == "") {
+//         this.parentNode.classList.remove('focusInput');
+//       }
+//     });
+//   });
+  
