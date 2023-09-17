b060c49a19a8855c99d5d209b0530658fc5bf5e2
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-09-17 21:47:56 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-# from .brand import ChooseBrandName
+from .qr_code import QrCode
 from .profile import CreateProfile
 from .user import User
 from .shorturl import Urlshort, generate_short_url, validate_url
