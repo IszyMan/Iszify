388bcb114eeb8ceba2775d3b57b82a0d5250e291
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-10-24 23:10:36 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from .qr_code import QrCode
+from .qr_code import QrCode, ScanData
 from .profile import CreateProfile
 from .user import User
 from .shorturl import Urlshort, generate_short_url, validate_url
