1a8c6cab3aae345f1dfae11cf87ab5ae762ed0ba
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-11-11 13:04:55 +0100

    Delete link2ru.sqlite3

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from .qr_code import QrCode
 from .profile import CreateProfile
 from .user import User
+# from .qrlib import QRCodeData
 from .shorturl import Urlshort, generate_short_url, validate_url
