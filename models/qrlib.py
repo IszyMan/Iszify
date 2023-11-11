819d33271e73afa4eb1a0bbf887ef8a6d31ca565
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-11-11 21:26:54 +0100

    Update qrlib.py

--- models/qrlib.py
+++ models/qrlib.py
@@ -1,6 +1,10 @@
-# from extensions import db
-#
-#
-# class QRCodeData(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     qr_code_image = db.Column(db.LargeBinary, nullable=False)
+from flask_login import UserMixin
+from extensions import db
+
+
+class QrcodeRecord(UserMixin, db.Model):
+    __tablename__ = "qr_code_record"
+    id = db.Column(db.Integer, primary_key=True)
+    qr_code_id = db.Column(db.Integer, db.ForeignKey("qr_code.id"))
+    date = db.Column(db.String(250))
+    clicks = db.Column(db.Integer, default=0)
