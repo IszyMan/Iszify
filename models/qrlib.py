0728c16564113daad7a909336f62540e63a0b290
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2023-11-23 13:51:14 +0100

    Update qrlib.py

--- models/qrlib.py
+++ models/qrlib.py
@@ -2,11 +2,10 @@ from flask_login import UserMixin
 from extensions import db
 from sqlalchemy.orm import relationship
 
+
 class QrcodeRecord(UserMixin, db.Model):
     __tablename__ = "qr_code_record"
     id = db.Column(db.Integer, primary_key=True)
     qr_code_id = db.Column(db.Integer, db.ForeignKey("qr_code.id"))
     date = db.Column(db.String(250))
     clicks = db.Column(db.Integer, default=0)
-    qr_code = relationship("QrCode", back_populates="qrcode_record")
-
