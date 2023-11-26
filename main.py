d7dde9daec49b808f999c06f629ba3f62b333966
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2023-11-26 15:09:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ def create_app():
     app = Flask(__name__)
 
     base_dir = os.path.dirname(os.path.realpath(__file__))
-    app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+    app.config["SECRET_KEY"] = "any-secret-key-you-choose"
     # The configuration for the URI of the database, the link2ru.db is the name of this project's dev database
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(base_dir, 'link2ru.sqlite3')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SERVER_NAME'] = os.getenv('SERVER_NAME', 'localhost:5000')
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+        base_dir, "link2ru.sqlite3"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SERVER_NAME"] = os.getenv("SERVER_NAME", "localhost:5000")
 
     login_manager.init_app(app)
     db.init_app(app)
