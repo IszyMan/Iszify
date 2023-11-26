8929100f2409655ccd906a8dd9a6ecf589ed64c9
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2023-11-26 15:09:17 +0100

    Update __init__.py

--- forms/__init__.py
+++ forms/__init__.py
@@ -20,22 +20,37 @@ class RegisterForm(FlaskForm):
     # first_name = StringField("First Name", validators=[DataRequired()])
     # last_name = StringField("Last Name", validators=[DataRequired()])
     username = StringField("User Name", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), validators.Email("This field requires a valid email "
-                                                                              "address")])
+    email = StringField(
+        "Email",
+        validators=[
+            DataRequired(),
+            validators.Email("This field requires a valid email " "address"),
+        ],
+    )
     password = PasswordField("Password", validators=[DataRequired()])
     # confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField("Sign Me Up!")
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email(), validators.Email()])
+    email = StringField(
+        "Email", validators=[DataRequired(), Email(), validators.Email()]
+    )
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Let Me In!")
 
 
 class CreatePostForm(FlaskForm):
-    linkname = StringField("Link Name ", validators=[DataRequired()], render_kw={"placeholder": "Link name"})
-    link = StringField("Twitter Url", validators=[DataRequired(), URL()], render_kw={"placeholder": "Enter your link"})
+    linkname = StringField(
+        "Link Name ",
+        validators=[DataRequired()],
+        render_kw={"placeholder": "Link name"},
+    )
+    link = StringField(
+        "Twitter Url",
+        validators=[DataRequired(), URL()],
+        render_kw={"placeholder": "Enter your link"},
+    )
 
     # categories = SelectField(u'Category', choices=[("Fashion", 'Fashion'), ("Travel", 'Travel')])
 
