0db080ebc6bd7e8e55b7081b0864b22a066dd637
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-05-24 15:54:04 +0100

    Update __init__.py

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,5 +1,6 @@
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_qrcode import QRcode
@@ -10,3 +11,4 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
+mail = Mail()
