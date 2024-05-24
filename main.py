9dc6a8bbe06a081564ec1b86fcb02b2bbf67f312
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-05-24 15:54:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 import base64
 from auth import auth_blp
-from extensions import login_manager, db, bootstrap, migrate, qr_code
+from extensions import login_manager, db, bootstrap, migrate, qr_code, mail
 from models import User
 from views.user_view import user_blp
 
@@ -18,9 +18,16 @@ def create_app():
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SERVER_NAME"] = os.getenv("SERVER_NAME", "localhost:5000")
+    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_PORT"] = 465
+    app.config["MAIL_USERNAME"] = "easytransact.send@gmail.com"
+    app.config["MAIL_PASSWORD"] = "xnueeryfwkbuqhou"
+    app.config["MAIL_USE_TLS"] = False
+    app.config["MAIL_USE_SSL"] = True
 
     login_manager.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
     bootstrap.init_app(app)
     qr_code.init_app(app)
     migrate.init_app(app, db)
