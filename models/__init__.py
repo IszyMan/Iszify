f719772aaf8c676315054f58ef837b4ab0af6cfe
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-06-28 20:36:22 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,6 @@ from .qr_code import QrCode, generate_short_url2
 from .profile import CreateProfile
 from .user import User
 from .qrlib import QrcodeRecord
-from .shorturl import Urlshort, generate_short_url, validate_url
+from .shorturl import Urlshort, generate_short_url, validate_url, save_url_clicks
 from .bio_link_entries import CreateBioLinkEntries
 from .create_bio_page import CreateBioPage
