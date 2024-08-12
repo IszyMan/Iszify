497b8a7ba942f5f08bafc064eddc3f30e8f6c64e
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-12 18:46:59 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -93,23 +93,6 @@ def generate_and_save_qr(data):
     return img_bytes_io.getvalue()
 
 
-# def generate_and_save_qr(data):
-#     qr = qrcode.QRCode(version=1, box_size=10, border=5)
-#     qr.add_data(data)
-#     qr.make(fit=True)
-#     img = qr.make_image(fill_color="black", back_color="white")
-#
-#     # Save the QR code to BytesIO
-#     img_bytes_io = BytesIO()
-#     img.save(img_bytes_io, format="PNG")
-#
-#     # Encode the image to base64
-#     img_bytes_io.seek(0)  # Reset the stream position to the start
-#     img_base64 = base64.b64encode(img_bytes_io.getvalue()).decode('utf-8')
-#
-#     return img_base64
-
-
 def update_qr_code(data, fill_color):
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
     qr.add_data(data)
