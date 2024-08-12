9487bbc593af7a3af14e24e4db9f211124721ffd
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-12 09:03:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))
+        return User.query.get(user_id)
 
     # 500 error handler
     @app.errorhandler(500)
