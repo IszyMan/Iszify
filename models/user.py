1270d90b5c733935ca9f8132869e6334049eaef7
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-12 09:03:56 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@ from extensions import db
 from sqlalchemy.orm import relationship
 from flask import request
 from random import randint
+from func import hex_id
 
 
 def generate_otp():
@@ -12,8 +13,7 @@ def generate_otp():
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    # brand_id = db.Column(db.Integer, db.ForeignKey("brand_name.brandname")) # NOT IN USE
+    id = db.Column(db.String(50), primary_key=True, default=hex_id)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     first_name = db.Column(db.String(100))
@@ -22,7 +22,6 @@ class User(UserMixin, db.Model):
     otp = db.Column(db.String(100), default=generate_otp)
     profile_link = db.Column(db.String(250), unique=True)
     email_verified = db.Column(db.Boolean, default=False)
-    # brand = relationship("ChooseBrandName", back_populates="brand") #THIS IS NOT IN USE
     brand_name = db.Column(db.String(100))
     bio_name = relationship("CreateBioPage", backref="author")
     posts = relationship("CreateProfile", backref="author")
