47ecb091433373272b4ddd6cea9da33bae7c41bc
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-12 09:03:44 +0100

    Update profile.py

--- models/profile.py
+++ models/profile.py
@@ -1,12 +1,13 @@
 from flask_login import UserMixin
 from extensions import db
 from sqlalchemy.orm import relationship
+from func import hex_id
 
 
 class CreateProfile(UserMixin, db.Model):
     __tablename__ = "users_links"
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    id = db.Column(db.String(50), primary_key=True, default=hex_id)
+    author_id = db.Column(db.String(50), db.ForeignKey("users.id"))
     description = db.Column(db.Text, default="")
     linkname = db.Column(db.String(250), nullable=False)
     link = db.Column(db.String(250), default="")
