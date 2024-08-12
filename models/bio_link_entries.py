f617c27683c73cc897da3f0b487938cf112f81c7
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-12 09:03:39 +0100

    Update bio_link_entries.py

--- models/bio_link_entries.py
+++ models/bio_link_entries.py
@@ -1,13 +1,14 @@
 from flask_login import UserMixin
 from extensions import db
 from sqlalchemy.orm import relationship
+from func import hex_id
 
 
 class CreateBioLinkEntries(UserMixin, db.Model):
     __tablename__ = "bio_links_entries"
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    bio_page_id = db.Column(db.String(250), db.ForeignKey("bio_page.id"))
+    id = db.Column(db.String(50), primary_key=True, default=hex_id)
+    author_id = db.Column(db.String(50), db.ForeignKey("users.id"))
+    bio_page_id = db.Column(db.String(50), db.ForeignKey("bio_page.id"))
     link_name = db.Column(db.String(250), nullable=False)
     link_url = db.Column(db.String(250), default="")
     clicks = db.Column(db.Integer, default=0)
