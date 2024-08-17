490d88477d7b795ded9648c04d0cab6dd23d4e35
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-17 22:50:47 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -7,3 +7,4 @@ from .shorturl import (Urlshort, generate_short_url,
 from .bio_link_entries import CreateBioLinkEntries
 from .create_bio_page import (CreateBioPage, save_bio_page_clicks, BioPageClicks,
                               get_bio_page_id, update_bio_page_clicks, BioPageClickLocation)
+from .posts import Posts, Categories, get_posts, get_categories, save_create_post, get_one_post
